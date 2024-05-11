2d4bb094c818790b1b1c113373a3094cee90a83f
Robear Mankaryous <rmankaryous12@gmail.com>
2024-05-11 13:19:15 -0700

    update max tokens to 1024

--- src/commands/chat.rs
+++ src/commands/chat.rs
@@ -119,7 +119,7 @@ async fn chat(
                     "top_p": 0.9,
                     "prompt": prompt,
                     "temperature": 0.2,
-                    "max_new_tokens": 512,
+                    "max_new_tokens": 1024,
                     "min_new_tokens": 0,
                     "stop_sequences": "<|im_end|>",
                     "prompt_template": "<|im_start|>system\\nYou\'re a helpful assistant<|im_end|>\\n<|im_start|>user\\n{prompt}<|im_end|>\\n\\n<|im_start|>assistant\\n",
